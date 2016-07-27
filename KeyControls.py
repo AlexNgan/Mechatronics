740dd720f7f0f95b6c90832ec12f3247bf6367bb
Gloria Ngan <ngang@bxscience.edu>
2016-07-27 15:08:00 -0400

    Add files via upload

---
+++ KeyControls.py
@@ -0,0 +1,31 @@
+#Author: Gloria
+#This program is a test for Python's curse functions.
+#Should print "Yay" when the up arrow key is struck.
+#Hit ctrl+c to exit.
+
+import sys
+import curses
+
+def init():
+    try:
+        stdscr = curses.initscr()
+        stdscr.clear()                #Clears screen.
+        curses.noecho()
+        curses.cbreak()             #Negates usual buffer mode.
+        stdscr.keypad(True)       #Enables keypad input.
+        
+        userSays = stdscr.getKey()
+        if userSays == KEY_UP:
+           print "Yay."
+    except KeyboardInterrupt:
+        #Will return terminal to regular state.
+        curses.nocbreak()
+        stdscr.keypad(False)    
+        curses.echo()
+        curses.endwin()
+        sys.exit()
+   userSays = stdscr.getKey()
+   if userSays == KEY_UP:
+       print "Yay."
+       
+init()
