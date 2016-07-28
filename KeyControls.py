03211ad71aa78a86f711daf5ff52598cac85f6f3
Gloria Ngan <ngang@bxscience.edu>
2016-07-28 09:46:02 -0400

    Update KeyControls.py
    Made changes. Removed extra code to pinpoint source of error.

--- KeyControls.py
+++ KeyControls.py
@@ -1,28 +1,21 @@
 #Author: Gloria
 #This program is a test for Python's curse functions.
-#Should print "Yay" when the up arrow key is struck.
-#Hit ctrl+c to exit.
 
 import sys
 import curses
 
-def init():
-    try:
-        stdscr = curses.initscr()
-        stdscr.clear()                #Clears screen.
-        curses.noecho()
-        curses.cbreak()             #Negates usual buffer mode.
-        stdscr.keypad(True)       #Enables keypad input.
-        
-        userSays = stdscr.getKey()
-        if userSays == KEY_UP:
-           print "Yay."
-    except KeyboardInterrupt:
-        #Will return terminal to regular state.
-        curses.nocbreak()
-        stdscr.keypad(False)    
-        curses.echo()
-        curses.endwin()
-        sys.exit()
-       
-init()
+#Configures console to work with arrow keys.
+def config():
+    stdscr = curses.initscr()
+    stdscr.clear()                #Clears screen.
+    curses.noecho()
+    curses.cbreak()             #Negates usual buffer mode.
+    stdscr.keypad(True)       #Enables keypad input.
+
+#Returns console to standard state.
+def revert():
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
+    sys.exit()
