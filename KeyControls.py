9cc1bd71c3761ba1da85268fa048e99dc19e655e
Gloria Ngan <ngang@bxscience.edu>
2016-07-28 10:24:31 -0400

    Update KeyControls.py
    Commented out extraneous lines for testing.

--- KeyControls.py
+++ KeyControls.py
@@ -1,28 +1,46 @@
-#Author: Gloria
-#This program is a test for Python's curse functions.
-
 import sys
 import curses
 from curses import wrapper
 
+#Author: Gloria
+#This program allows a robot to be controlled with arrow keys
+#through the Linux terminal.
+
+def drive(leftSpd, rightSpd):
+    #code here.
+
+#Returns console to standard state.
+def killSwitch():
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
+    sys.exit()
+
 #Configures console to work with arrow keys.
-def config():
+def init():
     try:        #Configures console to work with arrow keys.
         stdscr = curses.initscr()
         stdscr.clear()                #Clears screen.
         curses.noecho()
         curses.cbreak()             #Negates usual buffer mode.
-        stdscr.keypad(True)       #Enables keypad input.
-
-        userSays = stdscr.getch()
-        if userSays == curses.KEY_UP:
-            print ("Yay.")
+        stdscr.keypad(True)      #Enables keypad input.
+        while True:
+            userSays = stdscr.getch()
+            if userSays == 3:         #Should handle KeyboardInterrupt.
+                killSwitch()
+                break
+            elif userSays == curses.KEY_UP:
+                print("Hi.")
+            # elif userSays == curses.KEY_DOWN:
+            #     drive()
+            # elif userSays == curses.KEY_LEFT:
+            #     drive()
+            # elif userSays == curses.KEY_RIGHT:
+            #     drive()
+            # else:
+            #     drive()
     except KeyboardInterrupt:  #Returns console to standard state.
-        curses.nocbreak()
-        stdscr.keypad(False)
-        curses.echo()
-        curses.endwin()
-        sys.exit()
+        killSwitch()
 
-config()
-
+init()
